3dd03753f1703f2f69b70c7105c76bf77e141753
faadgaad780-boop <faadgaad780@gmail.com>
2025-09-17 17:50:40 +0300

    Delete api/api/env-check.js

--- api/api/env-check.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  return res.status(200).json({
-    ok: true,
-    hasOpenAIKey: Boolean(process.env.OPENAI_API_KEY),
-    modelSet: Boolean(process.env.OPENAI_MODEL),
-  });
-};
