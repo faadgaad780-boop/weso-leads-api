6904d6ff75108abbb655feb0907638be1892037c
faadgaad780-boop <faadgaad780@gmail.com>
2025-09-17 17:18:42 +0300

    Create env-check.js

---
+++ api/api/env-check.js
@@ -0,0 +1,7 @@
+module.exports = async (req, res) => {
+  return res.status(200).json({
+    ok: true,
+    hasOpenAIKey: Boolean(process.env.OPENAI_API_KEY),
+    modelSet: Boolean(process.env.OPENAI_MODEL),
+  });
+};
